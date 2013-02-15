15ca5bac37072556b258cdefb8561e334780ae53
Adrián Vázquez Sanisidro <adri93@gmail.com>
2013-02-15 12:35:02 +0100

    v1.2.2
    Commit inicial

---
+++ src/esi/delegacion/haydelegacion/Main.java
@@ -0,0 +1,265 @@
+package esi.delegacion.haydelegacion;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.ViewFlipper;
+
+public class Main extends Activity {
+
+	float init_x;
+
+	ViewFlipper flipper;
+	FrameLayout page1;
+	ScrollView page2, page3;
+	LinearLayout page4, page5, page6;
+
+	EditText mailBody;
+
+	ImageView toTwitter, toFacebook, toTuenti;
+
+	Button sendButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		flipper = (ViewFlipper) findViewById(R.id.mainFlipper);
+		page1 = (FrameLayout) findViewById(R.id.page1);
+		page2 = (ScrollView) findViewById(R.id.page2);
+		page3 = (ScrollView) findViewById(R.id.page3);
+		page4 = (LinearLayout) findViewById(R.id.page4);
+		page5 = (LinearLayout) findViewById(R.id.page5);
+		page6 = (LinearLayout) findViewById(R.id.page6);
+
+		sendButton = (Button) findViewById(R.id.enviar);
+		toTwitter = (ImageView) findViewById(R.id.totwitter);
+		toFacebook = (ImageView) findViewById(R.id.toface);
+		toTuenti = (ImageView) findViewById(R.id.toTuenti);
+
+		mailBody = (EditText) findViewById(R.id.mailbody);
+
+		page1.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+
+				flip(event);
+				return false;
+			}
+		});
+
+		page2.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+
+				flip2(event);
+				return false;
+			}
+		});
+
+		page3.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+
+				flip2(event);
+				return false;
+			}
+		});
+
+		page4.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+
+				flip(event);
+				return false;
+			}
+		});
+
+		page5.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+				flip2(event);
+				return false;
+			}
+		});
+
+		page6.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+				flip(event);
+				return false;
+			}
+		});
+
+		toTwitter.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				goToTwitter();
+
+			}
+		});
+
+		toFacebook.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				goToFacebook();
+
+			}
+		});
+
+		toTuenti.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				goToTuenti();
+
+			}
+		});
+
+		sendButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				sendEmail();
+
+			}
+		});
+
+		ArrayList<Tweet> tweets = null;
+		try {
+			tweets = Tweet.getTweets("esiHD", 1);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		if (tweets != null) {
+			ListView listView = (ListView) findViewById(R.id.ListViewId);
+			listView.setAdapter(new TweetItemAdapter(this,
+					R.layout.tweet_layout, tweets));
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void goToFacebook() {
+		Uri uri = Uri.parse("https://www.facebook.com/HayDelegacionEsiHd");
+		Intent i = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(i);
+	}
+
+	public void goToTwitter() {
+		Uri uri = Uri.parse("https://twitter.com/esiHD");
+		Intent i = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(i);
+	}
+
+	public void goToTuenti() {
+		Uri uri = Uri
+				.parse("http://www.tuenti.com/#m=Page&func=index&page_key=1_2702_62148087");
+		Intent i = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(i);
+	}
+
+	public void sendEmail() {
+		Intent i = new Intent(Intent.ACTION_SEND);
+		i.setType("plain/text");
+
+		String emaillist[] = { "haydelegacion@gmail.com" };
+
+		i.putExtra(Intent.EXTRA_EMAIL, emaillist);
+		i.putExtra(Intent.EXTRA_SUBJECT, "Dudas y/o sugerencias");
+		i.putExtra(Intent.EXTRA_TEXT, mailBody.getText().toString());
+
+		startActivity(i);
+	}
+
+	public void flip(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			init_x = event.getX();
+			break;
+
+		case MotionEvent.ACTION_UP:
+			float fin_x = event.getX();
+			if (init_x - fin_x > 0) {
+				Animation animationFlipIn = AnimationUtils.loadAnimation(this,
+						R.anim.flipin2);
+				Animation animationFlipOut = AnimationUtils.loadAnimation(this,
+						R.anim.flipout2);
+				flipper.setInAnimation(animationFlipIn);
+				flipper.setOutAnimation(animationFlipOut);
+				flipper.showNext();
+			}
+			if (init_x - fin_x < 0) {
+				Animation animationFlipIn = AnimationUtils.loadAnimation(this,
+						R.anim.flipin);
+				Animation animationFlipOut = AnimationUtils.loadAnimation(this,
+						R.anim.flipout);
+				flipper.setInAnimation(animationFlipIn);
+				flipper.setOutAnimation(animationFlipOut);
+				flipper.showPrevious();
+			}
+		}
+	}
+
+	public void flip2(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			init_x = event.getX();
+			break;
+
+		case MotionEvent.ACTION_UP:
+			float fin_x = event.getX();
+			if (init_x - fin_x > 100) {
+				Animation animationFlipIn = AnimationUtils.loadAnimation(this,
+						R.anim.flipin2);
+				Animation animationFlipOut = AnimationUtils.loadAnimation(this,
+						R.anim.flipout2);
+				flipper.setInAnimation(animationFlipIn);
+				flipper.setOutAnimation(animationFlipOut);
+				flipper.showNext();
+			}
+			if (init_x - fin_x < -100) {
+				Animation animationFlipIn = AnimationUtils.loadAnimation(this,
+						R.anim.flipin);
+				Animation animationFlipOut = AnimationUtils.loadAnimation(this,
+						R.anim.flipout);
+				flipper.setInAnimation(animationFlipIn);
+				flipper.setOutAnimation(animationFlipOut);
+				flipper.showPrevious();
+			}
+		}
+	}
+
+}
---
+++ src/esi/delegacion/haydelegacion/Tweet.java
@@ -0,0 +1,79 @@
+package esi.delegacion.haydelegacion;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+public class Tweet {
+	public String username;
+	public String message;
+	public String image_url;
+
+	public Tweet(String username, String message, String url) {
+		this.username = username;
+		this.message = message;
+		this.image_url = url;
+	}
+
+	public static ArrayList<Tweet> getTweets(String searchTerm, int page) throws JSONException {
+		String searchUrl = "http://search.twitter.com/search.json?q=@"
+				+ searchTerm + "&rpp=100&page=" + page;
+		
+		ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+	    HttpClient client = new DefaultHttpClient();
+	    HttpGet get = new HttpGet(searchUrl);
+
+	    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+	    String responseBody = null;
+	    try {
+	        responseBody = client.execute(get, responseHandler);
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+
+	    JSONObject jsonObject = new JSONObject(responseBody);
+
+	    JSONArray arr = null;
+
+	    try {
+	        arr = jsonObject.getJSONArray("results");
+	    } catch (Exception ex) {
+	        Log.v("TEST", "Exception: " + ex.getMessage());
+	    }
+
+	    for (int i = 0; i < arr.length(); i++) {
+	    	Tweet tweet = new Tweet(
+                    (arr.getJSONObject(i).get("from_user").toString()),
+                    (arr.getJSONObject(i).get("text").toString()),
+                    (arr.getJSONObject(i).get("profile_image_url").toString()));
+                    tweets.add(tweet);
+	    }
+
+	    return tweets;
+		
+	}
+
+	public static Bitmap getBitmap(String bitmapUrl) {
+		try {
+			URL url = new URL(bitmapUrl);
+			return BitmapFactory.decodeStream(url.openConnection()
+					.getInputStream());
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+}
